26d8ea1687723308b80abdd1836be2a26ce04aa1
fairoos <fairoosnm123@gmail.com>
2019-04-28 06:30:24 +0530

    Adds function for sign

--- digi_sign.py
+++ digi_sign.py
@@ -6,3 +6,20 @@ def take_image():
     image = Image.open("123.jpg")
     return image
 
+def digi_signature(image):
+    """sign in your image"""
+    draw = ImageDraw.Draw(image)
+    (x, y) = (1000, 700)
+    sign = "Demo sign"
+    color = "rgb(0, 0, 0)"
+    font = ImageFont.truetype("Roboto-Bold.ttf", size=45)
+    draw.text((x, y), sign, fill=color, font=font)
+    file_name_to_save = input("enter a file name you want to save : ")
+    image.save(file_name_to_save)
+
+def main():
+    image = take_image()
+    digi_signature(image)
+
+
+main()
